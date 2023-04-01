3a2d53b7e95a727809c16dea2fc0989690f64524
Vivek Kumar Shukla <vivekallahabadia541998mzp@gmail.com>
2023-04-01 19:30:55 +0530

    AdminLTE Html Integration

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+             @include('layouts.header')
+             @include('layouts.sidebar')
+             @yield('content')
+             @include('layouts.footer')
+</div>
+</body>
+</html>
